b35dd4035ee58fbae6384f8e935bc9527e79bc3e
RenzoPretto <43127379+RenzoPretto@users.noreply.github.com>
2019-10-02 20:31:09 -0400

    Delete config.js
    Getting rid of personal data, turning in on canvas.

--- Code/server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  db: {
-    uri: 'mongodb+srv://guest:cen3031access@cen3031-luldz.mongodb.net/cen3031?retryWrites=true&w=majority'//place the URI of your mongo database here.
-  }, 
-  openCage: {
-    key: '554e3a7054544d90b8429c8c80906d8a' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
-  },
-  port: 8080
-};
